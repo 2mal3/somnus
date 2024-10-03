f63b8a4f11e6eaf6998f21e89d07fd1dedd81e22
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-10-03 11:59:24 +0200

    ci: nix support

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      packages = with pkgs; [
+        openssl
+
+        python311
+        python311Packages.discordpy
+        python311Packages.python-dotenv
+        python311Packages.wakeonlan
+        python311Packages.ping3
+        python311Packages.pexpect
+        python311Packages.mcstatus
+        python311Packages.pydantic
+      ];
+    };
+  };
+}
