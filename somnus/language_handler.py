4c82e88cb19ac13db3b0882565991068a89c786e
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-02-24 16:56:28 +0100

    fix(languageHandler): wrong locale path

--- somnus/language_handler.py
+++ somnus/language_handler.py
@@ -3,4 +3,4 @@ from yai18n import Translator
 from somnus.config import CONFIG
 
 
-LH = Translator(fallback_locale="en", default_locale=CONFIG.LANGUAGE)
+LH = Translator(fallback_locale="en", default_locale=CONFIG.LANGUAGE, locale_folder_path="somnus/locales")
